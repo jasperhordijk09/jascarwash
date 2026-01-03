59ea3ccd4994b129ca1ff83734f73e1f0aad0980
jasperhordijk09 <jasperhordijk09@gmail.com>
2026-01-03 11:33:35 +0100

    logout en session-check verbetering

--- api/session-check.php
+++ api/session-check.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 
-// header("Access-Control-Allow-Origin: http://localhost:8081");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
