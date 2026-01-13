5637b230aa15dbc71afc3b4a25157c736aec4982
hordijkjasper6-byte <hordijkjasper6@gmail.com>
2026-01-13 13:10:39 +0100

    begin list users

--- frontend/src/src/pages/admin/appointments.vue
+++ frontend/src/src/pages/admin/appointments.vue
@@ -2,6 +2,10 @@
   <v-main>
     <router-view />
     <nav-bar />
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-calendar-month" title="my Appointments"></v-list-item>
+      <v-list-item prepend-icon="mdi-car" title="My Cars"></v-list-item>
+    </v-list>
   </v-main>
 </template>
 
--- frontend/src/src/pages/admin/users.vue
+++ frontend/src/src/pages/admin/users.vue
@@ -2,9 +2,18 @@
   <v-main>
     <router-view />
     <nav-bar />
+    <v-list>
+      <v-list-item prepend-icon="mdi-user" title={{appStore.me.username}}>
+        <v-list-item-subtitle>
+          {{ appStore.me.email }}
+          {{ appStore.me.full_name }}
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
   </v-main>
 </template>
 
 <script lang="ts" setup>
-
+import { useAppStore } from '@/stores/appStore';
+const appStore = useAppStore();
 </script>
